18669a9276fe3ff5c2e6b36750bc1fa8bba4a3ce
misawa-shota <kaiyou0120@gmail.com>
2026-06-07 23:22:44 +0900

    林道検索失敗時にトースターを表示する機能と検索実行後に検索欄を空にする機能を追加#21

--- resources/js/hooks/useSearchRindou.tsx
+++ resources/js/hooks/useSearchRindou.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 const useSearchRindou = () => {
     const [ searchRindouId, setSearchRindouId ] = useState<number | null>(null);
+    const [ searchStatus, setSearchStatus] = useState<string>("");
 
     const getSearchData = async (searchQuery: string) => {
         if (searchQuery.trim() === "") return;
@@ -11,15 +12,19 @@ const useSearchRindou = () => {
             const response = await axios.post("/header/search", {
                 searchQuery: searchQuery,
             });
-            console.log(response.data.id);
-            setSearchRindouId(response.data.id);
+            if (response.data.status === "search-success") {
+                setSearchRindouId(response.data.result.id);
+            } else if (response.data.status === "search-error") {
+                setSearchRindouId(null);
+                setSearchStatus("search-error");
+            }
         } catch (error: any) {
             console.error(error);
             console.error(error.response.data);
             console.error(error.response.status);
         }
     };
-    return { searchRindouId, getSearchData };
+    return { searchStatus, searchRindouId, getSearchData };
 };
 
 export default useSearchRindou;
